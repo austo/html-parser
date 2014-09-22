ac2034aa3875fd57adf48054ce4214a46049d777
Austin Moran <austin.moran@surveysampling.com>
2014-09-22 18:45:54 -0400

    broken recursive chapter parsing scheme

--- nrsv/chapter.go
+++ nrsv/chapter.go
@@ -2,6 +2,61 @@ package nrsv
 
 import (
 	"code.google.com/p/go.net/html"
+	"io"
+	// "io/ioutil"
 	"net/http"
-	"regexp"
+	// "regexp"
+	"fmt"
+	"testing"
 )
+
+func getRawVerseText(ch Chapter, t *testing.T) (*html.Node, error) {
+	res, err := http.Get(ch.url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return getTextNode(res.Body, t)
+	// bytes, err := ioutil.ReadAll(res.Body)
+	// if err != nil {
+	// 	return
+	// }
+	// text = string(bytes)
+	// return
+}
+
+func getTextNode(r io.Reader, t *testing.T) (n *html.Node, err error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return
+	}
+	for c := doc.FirstChild; c != nil; c = c.NextSibling {
+		if t != nil {
+			fmt.Printf("%#v\n", c)
+		}
+		if c.Type != html.ElementNode || c.Data != "div" {
+			continue
+		}
+		for _, a := range c.Attr {
+			if a.Key == "class" && a.Val == "passage-text" {
+				n = c
+				fmt.Printf("%#v\n", *n)
+				return
+			}
+		}
+	}
+	return
+}
+
+func findTextDiv(n *html.Node) (n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "div" {
+		for _, a := range n.Attr {
+			if a.Key == "class" && a.Val == "passage-text" {
+				return n
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findTextDiv(c)
+	}
+}
--- nrsv/chapter_test.go
+++ nrsv/chapter_test.go
@@ -1,1 +1,24 @@
 package nrsv
+
+import (
+	"testing"
+)
+
+var (
+	chap = Chapter{
+		"Genesis",
+		1,
+		"https://www.biblegateway.com/passage/?search=Genesis+1&version=NRSV"}
+)
+
+func TestGetChapterText(t *testing.T) {
+	text, err := getRawVerseText(chap, t)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(text)
+}
+
+// func TestGetTextNode(t *testing.T) {
+
+// }
